22eeb6ec5db569fdded325cab7c29abe56369cd5
AndreySinelnikov <55104290+AndreySinelnikov@users.noreply.github.com>
2021-09-28 11:10:04 +0300

    Delete JavaScript/Postman/GetValueOfPathVariable directory

--- JavaScript/Postman/GetValueOfPathVariable/PrerequestScript.js
+++
@@ -1,5 +0,0 @@
-pm.collectionVariables.set("pathVariableActualValue", pm.request.url.variables.get("pathVariable"));
-
-// The it can be retrieved in Tests with 
-// pm.collectionVariables.get("pathVariableActualValue");
-
--- JavaScript/Postman/GetValueOfPathVariable/Tests.js
+++
@@ -1,5 +0,0 @@
-pm.test("Your test name", function () {
-    var jsonData = pm.response.json();
-    var actualValue = pm.collectionVariables.get("actualValue"); // get request path variable
-    pm.expect(jsonData.value).to.eql(actualValue);
-});
