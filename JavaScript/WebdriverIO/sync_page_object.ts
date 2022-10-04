30b7e9c0f3b1a59ab3ad49a51528fa70ac5e0624
Andrey Sinelnikov <55104290+AndreySinelnikov@users.noreply.github.com>
2022-10-04 20:13:24 +0300

    Create sync_page_object.ts

---
+++ JavaScript/WebdriverIO/sync_page_object.ts
@@ -0,0 +1,30 @@
+class PageObject {
+  // Elements
+  //--------------------------------------------------------------------------------------------------------------------
+  get uniquePageElement(): WebdriverIO.Element {
+    const elem = $('unique-selector');
+    elem.waitForDisplayed(); // safety waits are built into getters
+    return elem;
+  }
+
+  get firstElement(): WebdriverIO.Element {
+    const elem = $$('multiple-result-selector')[0];
+    elem.waitForDisplayed();
+    return elem;
+  }
+
+  // Actions
+  //--------------------------------------------------------------------------------------------------------------------
+  actionEncompassingMultipleElements(): void {
+    this.uniquePageElement.scrollIntoView();
+    this.firstElement.click();
+  }
+
+  // Assertions
+  //--------------------------------------------------------------------------------------------------------------------
+  expectElementContainsText(text: string): void {
+    expect(this.uniquePageElement).toHaveTextContaining(text);
+  }
+}
+
+export default new PageObject();
